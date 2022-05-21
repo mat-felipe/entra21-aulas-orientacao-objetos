8a3bd84c26c95c468639387bbe8259bed0fc7bc1
Mateus Felipe <mateusgoettems@gmail.com>
2022-05-20 22:37:09 -0300

    Updated

--- Orientacao a objetos/src/br/com/entra21/orientacao/objeto/principal/Main.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objeto/principal/Main.java
@@ -5,31 +5,65 @@ import br.com.entra21.orientacao.objeto.principal.aula01.classes.Professor;
 
 import java.util.Scanner;
 
+
 public class Main {
 
+	// o objeto da classe Scanner esta na variavel de entrada, então é um obj
+	//porem o System.in é uma chamada static pq
+	//para acessar o in do System nao precisei dar new
 	static Scanner entrada = new Scanner(System.in);
-
+	static Scanner entrada2 = new Scanner(System.in);
 	public static void main(String[] args) {
+
 		
 		Professor professorJava = new Professor();
-		Professor professoraIngles = new Professor();
+		 
+		Professor professoraIngles = new Professor("Isabelle", (byte) 30);
 
-		Aluno alunoTeste = new Aluno();
-		Aluno outroAluno = new Aluno();
-		Aluno alunoNovato = new Aluno();
-		Aluno alunoObjeto;
-		alunoObjeto = new Aluno();
-		alunoObjeto.idade = 18;
+		System.out.println("Nome dela = "+professoraIngles.getNome());
+		
+		professoraIngles.setNome("Isabelle 2");
 
-		alunoTeste.nome = "Rubem";
-		alunoTeste.idade = 33;
-		outroAluno.idade = 18;
-		outroAluno.nome = "Visitante";
-		System.out.println("Qual o nome do aluno novato?");
-		alunoNovato.nome = entrada.next();
-		System.out.println("Qual a idade do " + alunoNovato.nome + "?");
-		alunoNovato.idade = entrada.nextByte();
-		System.out.println("Bem vindo " + alunoNovato.nome);
+		System.out.println("agora o nome  dela = "+professoraIngles.getNome());
+		System.out.println("A idade dela é "+professoraIngles.getIdade());
+		
+		System.out.println("Os professoes trabalham na "+Professor.instituicao);
+
+		Aluno alunoTeste= new Aluno(); 
+		
+		Aluno outroAluno= new Aluno();
+		
+		Aluno alunoNovato=new Aluno();
+		
+		Aluno alunoObjeto ;
+		
+		alunoObjeto= new Aluno();
+		
+		alunoObjeto.idade=18;
+		
+		 
+		alunoTeste.nome="Rubem";
+		alunoTeste.idade=33;
+		
+		outroAluno.idade=18;
+		outroAluno.nome="Visitante";
+
+		alunoTeste.responderChamada();
+		alunoTeste.responderChamada();
+		alunoTeste.responderChamada();
+		alunoTeste.responderChamada();
+		
+		/*
+		 * System.out.println("Qual o nome do aluno novato?");
+		 * alunoNovato.nome=entrada.next();
+		 * 
+		 * System.out.println("Qual a idade do "+alunoNovato.nome+"?");
+		 * alunoNovato.idade=entrada.nextByte();
+		 * 
+		 * System.out.println("Bem vindo "+alunoNovato.nome);
+		 */
+		
+		
 
 	}
 
--- Orientacao a objetos/src/br/com/entra21/orientacao/objeto/principal/aula01/classes/Professor.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objeto/principal/aula01/classes/Professor.java
@@ -2,19 +2,47 @@ package br.com.entra21.orientacao.objeto.principal.aula01.classes;
 
 public class Professor {
 	
-	private String nome;
-	private byte idade;
-	private byte quantidadedeAlunos;
+	//atributos static são de dominio da CLASSE e nao de cada objeto
+	//entao nemhum objeto dessa classe tem esse tipo de atributo
+	//para fazer acessos static basta informar a Classe.oqueVcquer, sem utilizar new
+	public static String instituicao="Proway";
 	
+	
+	
+	//atributos devem ser private e se possivel
+	//criar gets e sets para quem realmente precisa
+	private String nome;
+	private byte idadeAtual;
+	private byte quantidadeAlunos;
+	
+	
+	//new é criar objetos
 	public Professor() {
 		//construtor vazio nao inicializa os atributos
 	}
 	
-	public Professor(String novoNome, byte idade) {
-		this.nome=novoNome;
-		this.nome=idade;
-		//construtor com parametros
+	public Professor(String nome,byte idade) {
+		this.nome=nome;
+		 this.idadeAtual=idade;
+		// construtor com parametros
 		//pode inicializar alguns ou todos os atributos
 	}
+	
+	public String getNome() {
+		//ler o valor la fora
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		// receber um valor novo la de fora
+		this.nome=nome;
+	}
+	
+	public byte getIdade() {
+		return this.idadeAtual;
+	}
+	
+	 
+	
 
 }
