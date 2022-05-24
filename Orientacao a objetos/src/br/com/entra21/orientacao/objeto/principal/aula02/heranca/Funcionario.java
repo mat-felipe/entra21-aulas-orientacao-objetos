bcbb452e35be7c62cc5ebe10e9628ad9f43a9462
Mateus Felipe <mateusgoettems@gmail.com>
2022-05-24 20:32:30 -0300

    Updated

---
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objeto/principal/aula02/heranca/Funcionario.java
@@ -0,0 +1,21 @@
+package br.com.entra21.orientacao.objeto.principal.aula02.heranca;
+
+public class Funcionario {
+
+	private String nome;
+	private byte idade;
+	private String cargoAtual;
+	private float salario;
+
+	public Funcionario() {
+
+	}
+
+	public Funcionario(String nome, byte idade, String cargo, float valor) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cargoAtual = cargo;
+		this.salario = valor;
+
+	}
+}
